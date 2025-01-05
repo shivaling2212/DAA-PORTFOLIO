44ab21dd3e424b4593bae2c21b5092945d7bd3a4
shivaling2212 <146571705+shivaling2212@users.noreply.github.com>
2025-01-06 01:26:37 +0530

    Update floyds5.cpp

--- codes/floyds5.cpp
+++ codes/floyds5.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <climits>
 
+using namespace std;
+
+void floydWarshall(int V, vector<vector<int>>& graph) {
+    vector<vector<int>> dist = graph;
+
+    for (int k = 0; k < V; ++k) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][k] != INT_MAX && dist[k][j] != INT_MAX && dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    cout << "Shortest energy load paths between classrooms:\n";
+    for (int i = 0; i < V; ++i) {
+        for (int j = 0; j < V; ++j) {
+            if (dist[i][j] == INT_MAX) {
+                cout << "INF\t";
+            } else {
+                cout << dist[i][j] << "\t";
+            }
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int V = 4;
+    vector<vector<int>> graph = {
+        {0, 3, INT_MAX, 7},
+        {3, 0, 1, INT_MAX},
+        {INT_MAX, 1, 0, 2},
+        {7, INT_MAX, 2, 0}
+    };
+
+    floydWarshall(V, graph);
+    return 0;
+}
