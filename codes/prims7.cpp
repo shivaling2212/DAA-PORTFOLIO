ddc46bf922d6a2a2b5ce9aa84e8d2ddf2f5111d6
shivaling2212 <146571705+shivaling2212@users.noreply.github.com>
2025-01-06 01:27:11 +0530

    Update prims7.cpp

--- codes/prims7.cpp
+++ codes/prims7.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <climits>
 
+using namespace std;
+
+int minKey(const vector<int>& key, const vector<bool>& mstSet, int V) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < V; v++) {
+        if (!mstSet[v] && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void primMST(const vector<vector<int>>& graph, int V) {
+    vector<int> parent(V, -1);
+    vector<int> key(V, INT_MAX);
+    vector<bool> mstSet(V, false);
+
+    key[0] = 0;
+
+    for (int count = 0; count < V - 1; count++) {
+        int u = minKey(key, mstSet, V);
+
+        mstSet[u] = true;
+
+        for (int v = 0; v < V; v++) {
+            if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    cout << "Edge \tWeight\n";
+    for (int i = 1; i < V; i++) {
+        cout << parent[i] << " - " << i << " \t" << graph[i][parent[i]] << endl;
+    }
+}
+
+int main() {
+    int V = 5;
+    vector<vector<int>> graph = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+
+    primMST(graph, V);
+
+    return 0;
+}
